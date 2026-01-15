0a26bc1b13511926c955873b22695c30286db4be
goal-Point <goal-point@outlook.com>
2026-01-15 11:08:40 +0000

    Add pages router homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>KNGO HOME PAGE WORKING</h1>
+      <p>If you can see this, Pages Router is active.</p>
+    </div>
+  );
+}
